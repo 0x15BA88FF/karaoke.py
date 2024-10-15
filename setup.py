557bf40c1376e872eb4683b0b36c0ee0dd1164cf
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2024-10-15 20:07:21 +0000

    Update setup meta data

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup, find_packages
 
+VERSION = "0.0.0"
+NAME = "Karaoke"
+DESCRIPTION = " "
+LONG_DESCRIPTION = """ """
+
 setup(
-    name="Karaoke.py",
-    version="0.0.0",
+    name=NAME,
+    version=VERSION,
+    author="0x15BA88FF",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    keywords=["karaoke", "music", "lrc"]
     packages=find_packages(),
 )
